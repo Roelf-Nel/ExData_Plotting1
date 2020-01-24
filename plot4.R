04e3e27ff28e47c5a42deaa81e697e4ead41060a
Roelf-Nel <55146895+Roelf-Nel@users.noreply.github.com>
2020-01-24 12:00:22 +0200

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -12,9 +12,19 @@ energy.filtered$DateTime <- as.POSIXct(paste(energy.filtered$Date, energy.filter
 
 ## construct the plot
 
-png("plot3.png", width=480, height=480)
+png("plot4.png", width=480, height=480)
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+
 with(energy.filtered,
-{     
+{
+   plot(Global_active_power~DateTime, type="l", 
+        ylab="Global Active Power (kilowatts)", 
+        xlab="")
+   
+   plot(Voltage~DateTime, type="l", 
+        ylab="Voltage (volt)", 
+        xlab="")
+   
    plot(Sub_metering_1, 
         type="l",
         ylab="Global Active Power (kilowatts)", 
@@ -23,10 +33,14 @@ with(energy.filtered,
         col="red")
    lines(Sub_metering_3, 
         col="blue")
+   legend("topright", 
+          col=c("black", "red", "blue"), 
+          lty=1, 
+          lwd=2, 
+          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+   
+   plot(Global_reactive_power~DateTime, type="l", 
+        ylab="Global Rective Power (kilowatts)",xlab="")
 })
-legend("topright", 
-       col=c("black", "red", "blue"), 
-       lty=1, 
-       lwd=2, 
-       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
 dev.off()
